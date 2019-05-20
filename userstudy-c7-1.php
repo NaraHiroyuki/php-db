cd5b7a230165bf96c36a134b4a86840e735489fc
NaraHiroyuki <hiroyuki0818yakyu@icloud.com>
2019-05-20 10:16:51 +0900

    Memberクラスを作成

--- userstudy-c7-1.php
+++ userstudy-c7-1.php
@@ -30,8 +30,8 @@ $taro->hello();
 
 //p225
 require_once("userstudy-c7.Staff.php");
-$hana = new Menber("花", 21);
-$taro = new Menber("太朗", 35);
+$hana = new Member("花", 21);
+$taro = new Member("太朗", 35);
 
 $hana->hello();
 $taro->hello();
